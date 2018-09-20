b332c3f182adc0cd70148d1e964a452057cfc399
Timinis <litimc3@gmail.com>
2018-09-20 10:14:50 -0700

    sql setup

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-'use strict'
+'use strict';
 // These variables create the connection to the dependencies.
 const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
+const pg = require('pg');
+
 const app = express();
 
 // Tells express to use 'cors' for cross-origin resource sharing
@@ -53,8 +55,11 @@ function MovieResults(movie) {
 
 // Google helper function refactored prior to lab start.
 function getLocation(request, response) {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GOOGLE_API_KEY}`;
-  return superagent.get(url)
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${
+    request.query.data
+  }&key=${process.env.GOOGLE_API_KEY}`;
+  return superagent
+    .get(url)
     .then(location => {
       response.send(new LocationResult(request.query.data, location));
     })
@@ -71,13 +76,16 @@ function LocationResult(search, location) {
 
 // Weather helper function
 function getWeather(request, response) {
-  const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-  return superagent.get(url)
+  const url = `https://api.darksky.net/forecast/${
+    process.env.DARK_SKY_API_KEY
+  }/${request.query.data.latitude},${request.query.data.longitude}`;
+  return superagent
+    .get(url)
     .then(result => {
       let weatherData = [];
-      weatherData = result.body.daily.data.map((weather) => {
-        return new WeatherResult(weather)
-      })
+      weatherData = result.body.daily.data.map(weather => {
+        return new WeatherResult(weather);
+      });
       response.send(weatherData);
     })
     .catch(error => processError(error, response));
@@ -85,15 +93,18 @@ function getWeather(request, response) {
 
 // Restraurant helper function
 function getRestaurants(request, response) {
-  const url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`;
+  const url = `https://api.yelp.com/v3/businesses/search?location=${
+    request.query.data.search_query
+  }`;
 
-  return superagent.get(url)
+  return superagent
+    .get(url)
     .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
     .then(result => {
       let yelpData = [];
-      yelpData = result.body.businesses.map((restaurant) => {
+      yelpData = result.body.businesses.map(restaurant => {
         return new RestaurantResult(restaurant);
-      })
+      });
       response.send(yelpData);
     })
     .catch(error => processError(error, response));
@@ -101,13 +112,16 @@ function getRestaurants(request, response) {
 
 //Movies helper function
 function getMovies(request, response) {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_APIv3_KEY}&query=${request.query.data.search_query}`
-  return superagent.get(url)
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${
+    process.env.TMDB_APIv3_KEY
+  }&query=${request.query.data.search_query}`;
+  return superagent
+    .get(url)
     .then(result => {
       let movieData = [];
       movieData = result.body.results.map(movie => {
         return new MovieResults(movie);
-      })
+      });
       response.send(movieData);
     })
     .catch(error => processError(error, response));
